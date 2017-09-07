f92f5bda9bda0630cad78a2401d8a99dc1d9f16c
Ramil Nugmanov <stsouko@live.ru>
2017-09-08 00:26:42 +0300

    redis worker api fixed.

--- redis_worker.py
+++ redis_worker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  Copyright 2016, 2017 Ramil Nugmanov <stsouko@live.ru>
+#  Copyright 2016-2018 Ramil Nugmanov <stsouko@live.ru>
 #  This file is part of ModelManager.
 #
 #  ModelManager is free software; you can redistribute it and/or modify
@@ -29,13 +29,14 @@ from tempfile import mkdtemp
 from traceback import format_exc
 
 
-def run(structures=None, model=None):
+def run(structures, model):
     """
     model runner
     :param structures: list of {data: chemical structure (smiles mrv etc), pressure: float, temperature: float,
-                                additives: [{name: str, amount: float, type: AdditiveType(Enum)}], structure: int,
-                                status: MWUI.constants[StructureStatus], type: MWUI.constants[StructureType]} 
-    :param model: dict(name=str, description: str, type: ModelType(Enum), model=int)
+                                additives: [{name: str, amount: float, type: MWUI.constants[AdditiveType(Enum)]}],
+                                structure: int, description: [{key: str, value: str}],
+                                status: MWUI.constants[StructureStatus], type: MWUI.constants[StructureType]}
+    :param model: model name
     :return: updated list of structure dicts. new key - models: [dict(result: [dict,], **model arg)]
     
     if model not found or crashed return data without results in models[0]
@@ -47,7 +48,7 @@ def run(structures=None, model=None):
         return structures
 
     workpath = mkdtemp(dir=WORKPATH)
-    mod = ModelSet().load_model(model['name'], workpath=workpath)
+    mod = ModelSet().load_model(model, workpath=workpath)
 
     if mod is None:
         print('Model not found or not loadable')
@@ -56,7 +57,7 @@ def run(structures=None, model=None):
 
     try:
         results = mod.get_results([x.copy() for x in structures])
-    except Exception:
+    except:
         print('Model not working:\n %s' % format_exc(), file=stderr)
         rmtree(workpath)
         return fail_prep('Model not working')
@@ -68,25 +69,25 @@ def run(structures=None, model=None):
     if len(results) != len(structures) and mod.get_type() not in (ModelType.MOLECULE_SEARCHING,
                                                                   ModelType.REACTION_SEARCHING):
         print('Model lost structures. check model code for correctness')
-        return fail_prep('Model not working')
+        return fail_prep('Model lost data')
 
     if mod.get_type() in (ModelType.REACTION_MODELING, ModelType.MOLECULE_MODELING):
         if any(s[x] != r[x] for s, r in zip(structures, results)
-               for x in ('data', 'structure', 'status', 'type', 'temperature', 'pressure', 'additives')):
+               for x in ('data', 'structure', 'status', 'type', 'temperature', 'pressure', 'additives', 'description')):
             print("Editing structure, properties and meta denied! ONLY results assign possible!")
-            return fail_prep('Model not working')
+            return fail_prep('Model broke data')
 
     if mod.get_type() == ModelType.PREPARER:
-        if any(s[x] != r[x] for s, r in zip(structures, results) for x in ('structure', 'additives')):
+        if any(s[x] != r[x] for s, r in zip(structures, results) for x in ('structure', 'additives', 'description')):
             print("Editing meta denied! "
                   "ONLY results assigning, data, temperature, pressure type nd status editing possible!")
-            return fail_prep('Model not working')
+            return fail_prep('Preparing model not working')
 
     return results
 
 
-def convert(structures=None, model=None):
+def convert(structures, model):
     tmp = convert_upload(structures)
     if not tmp:
         raise Exception('File converter failed')
-    return run(structures=tmp, model=model)
+    return run(tmp, model)
